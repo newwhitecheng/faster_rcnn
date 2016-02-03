9b2af19c9c75b02cf8c7c89efd3ed27bb4da6072
Hao <jacky55062003@gmail.com>
2016-02-03 17:35:41 +0800

    clear some printer.

--- lib/datasets/factory_imagenet.py
+++ lib/datasets/factory_imagenet.py
@@ -21,23 +21,10 @@ def _selective_search_IJCV_top_k(split, year, top_k):
     imdb.config['top_k'] = top_k
     return imdb
 
-# Set up voc_<year>_<split> using selective search "fast" mode
 for split in ['train', 'val', 'val1', 'val2', 'test']:
     name = 'imagenet_{}'.format(split)
     devkit_path = '/home/Hao/Work/cvpr16/data/ILSVRC13'
     __sets[name] = (lambda split=split, devkit_path = devkit_path:datasets.imagenet.imagenet(split,devkit_path))
-    print name
-    print __sets[name]
-
-'''
-# Set up voc_<year>_<split>_top_<k> using selective search "quality" mode
-# but only returning the first k boxes
-for top_k in np.arange(1000, 11000, 1000):
-    for year in ['2007', '2012']:
-        for split in ['train', 'val', 'trainval', 'test']:
-            name = 'voc_{}_{}_top_{:d}'.format(year, split, top_k)
-            __sets[name] = (lambda split=split, year=year, top_k=top_k:_selective_search_IJCV_top_k(split, year, top_k))
-'''
 
 def get_imdb(name):
     """Get an imdb (image database) by name."""
